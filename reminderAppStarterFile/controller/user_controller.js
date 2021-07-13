dc6178d408c76953712cd7e768da131a3288c53f
Sydney L <riko1887@gmail.com>
2021-07-12 21:26:26 -0700

    middleware and other files added

---
+++ reminderAppStarterFile/controller/user_controller.js
@@ -0,0 +1,26 @@
+const userModel = require("../models/userModel").userModel;
+
+let database = require("../database")
+
+const getUserByEmailAndPassword = (email, password) => {
+    let user = userModel.findOne(email);
+    if (user) {
+        if (user.password === password) {
+            return user;
+        }
+    }
+    return null;
+};
+
+const getUserByID = (id) => {
+    let user = userModel.findById(id);
+    if (user) {
+        return user;
+    }
+    return null;
+};
+
+module.exports = {
+    getUserByEmailAndPassword,
+    getUserByID,
+}
---
+++ reminderAppStarterFile/middleware/passport.js
@@ -0,0 +1,33 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const userController = require("../controller/user_controller");
+const localLogin = new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (email, password, done) => {
+      console.log("User is logged in!")
+      const user = userController.getUserByEmailAndPassword(email, password);
+      return user
+        ? done(null, user)
+        : done(null, false, {
+            message: "Your login details are not valid. Please try again",
+          });
+    }
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    let user = userController.getUserByID(id);
+    if (user) {
+        done(null, user);
+    } else {
+        done({ message: "User not found" }, null);
+    }
+});
+
+module.exports = passport.use(localLogin);
